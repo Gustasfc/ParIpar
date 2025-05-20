c86304fc1852b84b9d521bfce94155350e9e671a
Gustavo Gonzalez <gustavorgonzalezgja@gmal.com>
2025-05-19 23:47:15 -0300

    parimpar

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿double numeroDigitado;
+double restoDivisaoPorDois;
+
+Console.Clear();
+
+Console.WriteLine("Digite um número: ");
+numeroDigitado = Convert.ToDouble(Console.ReadLine());
+
+restoDivisaoPorDois = numeroDigitado % 2;
+
+if (restoDivisaoPorDois != 0)
+{
+    Console.WriteLine($"O número {numeroDigitado} é ímpar!");
+}
+else
+{
+    Console.WriteLine($"O número {numeroDigitado} é par!");
+}
